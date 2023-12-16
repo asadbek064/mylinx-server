0a6ddafed3da9eb44a7bd954e688fe22d2b8d6da
Asad <contact@asadbek.dev>
2023-12-16 15:05:19 -0500

    images are not saved istead piped around until upload to backblaze

--- src/utils/backblaze.ts
+++ src/utils/backblaze.ts
@@ -1,14 +1,16 @@
 import B2 from 'backblaze-b2';
-import type { UploadedFile } from 'express-fileupload';
 import nanoid from 'nanoid';
+import { processImage } from './processimage';
+import { readFile } from 'fs/promises';
+import { parseHrtimeToSeconds } from './util';
 
 export type BackBlaze = {
   url: string;
 };
 
-export const uploadFile = async <T extends Pick<UploadedFile, 'data' | 'name'>>(
-  file: T
-) => {
+export const uploadFile = async (fileBuffer: Buffer, mimeType: string) => {
+  const startImageProcess = process.hrtime();
+
   const applicationKeyId = process.env['BACKBLAZE_KEY_ID'] as string;
   const applicationKey = process.env['BACKBLAZE_APP_KEY'] as string;
   const bucketId = process.env['BACKBLAZE_BUCKET_ID'] as string;
@@ -18,6 +20,14 @@ export const uploadFile = async <T extends Pick<UploadedFile, 'data' | 'name'>>(
   const fileName = `mylinx-img-${fileId}`;
 
   try {
+    // process image before upload
+    const processedFileBuffer = await processImage(fileBuffer);
+
+    const elapsedImageSeconds = parseHrtimeToSeconds(
+      process.hrtime(startImageProcess)
+    );
+    console.log('image process: ' + elapsedImageSeconds + 'seconds');
+
     const b2 = new B2({ applicationKeyId, applicationKey });
 
     const { data: authData } = await b2.authorize();
@@ -25,13 +35,12 @@ export const uploadFile = async <T extends Pick<UploadedFile, 'data' | 'name'>>(
       bucketId: bucketId,
     });
 
-    const body = file.data;
-
     const { data: uploadedFile } = await b2.uploadFile({
       uploadUrl: uploadData.uploadUrl,
       uploadAuthToken: uploadData.authorizationToken,
-      data: body,
+      data: processedFileBuffer,
       fileName: fileName,
+      mime: mimeType,
     });
 
     // Construct friendly URL to file
